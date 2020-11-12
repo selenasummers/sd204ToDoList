85c1a74027aaa9c0876ec79f82ff5bf6c0a32aa1
1styrGroupC <1styrGroupC@pn.local>
2020-11-12 18:06:35 +0800

    second commit

--- app.js
+++ app.js
@@ -9,3 +9,36 @@ app.use('/static', express.static(path.join(__dirname, 'src')));
 app.set("view engine", "ejs");
 
 let values = [{}];
+
+app.get("/home", (request, respond) => {
+    respond.send("Home Page");
+});
+
+app.get("/", (request, respond) => {
+    respond.render("index", { data: values });
+});
+
+app.post("/delete", urlEncodedParser, (request, respond) => {
+    let{id} = request.body
+    values.splice(id, 1)
+    respond.redirect("/");
+});
+
+app.get("/test", (request, respond) => {
+    respond.render("Test Page");
+});
+
+app.post("/post", urlEncodedParser, (request, respond) => {
+    values.push(request.body);
+    respond.redirect("/");
+});
+
+app.get("/about", (request, respond) => {
+    respond.send("About Page");
+});
+
+app.get("*", (request, respond) => {
+    respond.send("404 Page");
+});
+
+app.listen(1485);
